5d480f15713acf421efde44f64cd706a06d6d583
Sam Chillcott <sam@secret-source.eu>
2020-10-21 14:48:16 +0100

    reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        { title: 'No Scrubs', duration: '4.05'},
+        { title: 'No Way', duration: '2.15'},
+        { title: 'No Change', duration: '3.45'},
+        { title: 'No No No', duration: '2.55'},
+    ];
+};
+
+const selectedSongReducer = (selectedSong=null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+}
