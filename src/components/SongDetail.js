4d8f198e7c40b4afeddf3cc947923f238f92ef11
Sam Chillcott <sam@secret-source.eu>
2020-10-22 12:22:50 +0100

    SongDetail

---
+++ src/components/SongDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+const SongDetail = (props) => {
+    console.log(props);
+    return (
+        <div>
+            Song Detail
+        </div>
+    );
+}
+
+const mapStateToProps = (state) => {
+    return { song: selectedSong };
+};
+
+export default connect(mapStateToProps)(SongDetail)
+
+
+
