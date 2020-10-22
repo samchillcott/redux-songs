1d2df9eaea4e4092670f1a6ab5343fe9a116071b
Sam Chillcott <sam@secret-source.eu>
2020-10-22 11:52:30 +0100

    selectSong to connect

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectSong } from '../actions/index';
 
 class SongList extends Component {
+    renderList() {
+        return this.props.songs.map((song) => {
+            return (
+                <div className="item" key={ song.title }>
+                    <div className="right floated content">
+                        <button onClick={() => this.props.selectSong(song)}className="ui primary button">
+                            Select
+                            </button>
+                    </div>
+                    <div className="content">{ song.title }</div>
+                </div>
+            );
+        })
+    }
     render() {
-        console.log(this.props);
-        return (
-            <div>
-                SongList
-            </div>
-        )
+        return <div className="ui divided list">{this.renderList()}</div>
     }
 }
 
@@ -16,4 +26,4 @@ const mapStateToProps = (state) => {
     return { songs: state.songs };
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {selectSong})(SongList);
